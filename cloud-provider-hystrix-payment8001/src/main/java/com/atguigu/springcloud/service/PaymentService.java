840179aa72921342ba26eab59c0d6367aee0b852
ices <2388627880@qq.com>
2020-05-26 20:28:59 +0800

    p50 Hystrix支付微服务构建

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+    public String paymentInfo_OK(Integer id){
+        return "线程池："+Thread.currentThread().getName()+"paymentInfo_OK,id:"+id;
+    }
+
+    public String paymentInfo_TimeOut(Integer id){
+        int timeNumber=3;
+
+        try{
+            TimeUnit.SECONDS.sleep(timeNumber);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return "线程池："+Thread.currentThread().getName()+"paymentInfo_TimeOut,id:"+id+"\t 耗时"+timeNumber+"秒";
+    }
+}

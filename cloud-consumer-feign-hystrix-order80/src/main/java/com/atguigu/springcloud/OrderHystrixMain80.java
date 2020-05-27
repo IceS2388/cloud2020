d9496916e27f276b042ed65c2cb063478702e844
ices <2388627880@qq.com>
2020-05-27 11:56:13 +0800

    p52 applicaion.yml中的配置，最好采用复制的方式。手敲容易出错，错误不容易查找出。

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixMain80.java
@@ -3,13 +3,12 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentHystrixMain8001 {
+@EnableFeignClients
+public class OrderHystrixMain80 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentHystrixMain8001.class,args);
+        SpringApplication.run(OrderHystrixMain80.class,args);
     }
-
-
 }

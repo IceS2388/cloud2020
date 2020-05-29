e4670d1f6ff78977022c24eb61980ecc65b90214
ices <2388627880@qq.com>
2020-05-29 11:41:02 +0800

    p77 手动刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
@@ -1,10 +1,12 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //配置注解，实现刷新 curl -X POST "http://localhost:3355/actuator/refresh"
 public class ConfigController {
 
     @Value("${config.info}")

88f27af67b86f7e14a2070a9aa379d45f65e3f55
ices <2388627880@qq.com>
2020-06-08 11:36:22 +0800

    p135 Sentinel exceptionToIgnore

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entites.CommonResult;
 import com.atguigu.springcloud.entites.Payment;
 import lombok.extern.slf4j.Slf4j;
@@ -21,20 +22,34 @@ public class CircleBreakerController {
     @Resource
     private RestTemplate restTemplate;
 
+
+    //@SentinelResource("fallback")//什么都没配置
+    //@SentinelResource(value = "fallback",fallback = "handlerFallback")//只配置fallback
+//    @SentinelResource(value = "fallback", blockHandler = "blockHandler")
+    @SentinelResource(value="fallback",fallback = "handlerFallback",blockHandler = "blockHandler")
     @GetMapping("/consumer/fallback/{id}")
-    @SentinelResource("fallback")
-    public CommonResult<Payment> fallback(@PathVariable Long id){
+    public CommonResult<Payment> fallback(@PathVariable Long id) {
         log.info(SERVICE_URL);
 
-        CommonResult<Payment> result=restTemplate.getForObject(SERVICE_URL+"/paymentSQL/"+id,CommonResult.class,id);
+        CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
 
-        if(id==4){
+        if (id == 4) {
             throw new IllegalArgumentException("IllegalArgumentException,非法参数异常...");
-        }else if(result.getData()==null){
+        } else if (result.getData() == null) {
             throw new NullPointerException("NullPointException,该ID没有对应记录，空指针异常");
         }
 
         return result;
-
     }
+
+    public CommonResult<Payment> handlerFallback(@PathVariable Long id, Throwable exception) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(444, "异常兜底方法,exception:" + exception.getMessage(), payment);
+    }
+
+    public CommonResult<Payment> blockHandler(@PathVariable Long id, BlockException exception) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(445, "blockHandler限流兜底方法,exception:" + exception.getMessage(), payment);
+    }
+
 }
